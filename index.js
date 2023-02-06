8105cdcbffaf915fa164d92c1a0eeb132c69ccdb
Val <102326092+vdeva@users.noreply.github.com>
2023-02-06 12:44:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -126,8 +126,8 @@ async function get_followers() {
       ).then(() => {
         const follower_avatar = {
           input: `${follower.screen_name}.png`,
-          top: parseInt(`${380 + 300 * index}`),
-          left: 0, //3950
+          top: 650, //parseInt(`${380 + 300 * index}`)
+          left: parseInt(`${1850 + 400 * index}`), //3950
         };
         image_data.push(follower_avatar);
         count++;
